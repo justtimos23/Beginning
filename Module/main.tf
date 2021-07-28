69ce4e99f0bf2319c8db1ba8d725b1d16abc2c37
Joel talabi <joeltalabi10@gmail.com>
2021-07-28 10:42:35 -0400

    added vnet

--- Module/main.tf
+++ Module/main.tf
@@ -6,3 +6,29 @@ resource "azurerm_resource_group" "groupon"
   name     = "groupon-rg"
   location = "eastus"
 }
+
+resource "azurerm_virtual_network" "groupon" {
+  name                = "groupon-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.groupon.location
+  resource_group_name = azurerm_resource_group.groupon.name
+}
+
+resource "azurerm_subnet" "groupon" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.groupon.name
+  virtual_network_name = azurerm_virtual_network.groupon.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "groupon" {
+  name                = "groupon-nic"
+  location            = azurerm_resource_group.groupon.location
+  resource_group_name = azurerm_resource_group.groupon.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.groupon.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
